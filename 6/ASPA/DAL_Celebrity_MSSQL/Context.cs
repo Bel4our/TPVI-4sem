256bfb9218df52a7a693015d4c30ee482c2e3298
Daniil <daniilsemenov435687@gmail.com>
2025-05-31 18:50:49 +0300

    sixth lab added

---
+++ 6/ASPA/DAL_Celebrity_MSSQL/Context.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Celebrity_MSSQL
+{
+    public class Context : DbContext
+    {
+        public string? ConnectionString { get; private set; } = null;
+        public Context() : base() { }
+        public Context(string connstring) : base()
+        {
+            this.ConnectionString = connstring;
+        }
+        public DbSet<Celebrity> Celebrities { get; set; }
+        public DbSet<Lifeevent> Lifeevents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                if (this.ConnectionString == null)
+                {
+                    optionsBuilder.UseSqlServer("Data Source=DESKTOP-P1GMMIV; Initial Catalog=LESO1; Integrated Security=True; TrustServerCertificate=True;");
+                }
+                else
+                {
+                    optionsBuilder.UseSqlServer(this.ConnectionString);
+                }
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Celebrity>().ToTable("Celebrities").HasKey(p => p.Id);
+            modelBuilder.Entity<Celebrity>().Property(p => p.Id).IsRequired();
+            modelBuilder.Entity<Celebrity>().Property(p => p.FullName).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Celebrity>().Property(p => p.Nationality).IsRequired().HasMaxLength(2);
+            modelBuilder.Entity<Celebrity>().Property(p => p.ReqPhotoPath).HasMaxLength(200);
+
+            modelBuilder.Entity<Lifeevent>().ToTable("Lifeevents").HasKey(p => p.Id);
+            modelBuilder.Entity<Lifeevent>().Property(p => p.Id).IsRequired();
+            modelBuilder.Entity<Lifeevent>().Property(p => p.CelebrityId).IsRequired();
+            modelBuilder.Entity<Lifeevent>()
+                .HasOne<Celebrity>()
+                .WithMany()
+                .HasForeignKey(p => p.CelebrityId)
+                .IsRequired();
+            modelBuilder.Entity<Lifeevent>().Property(p => p.Date);
+            modelBuilder.Entity<Lifeevent>().Property(p => p.Description).HasMaxLength(256);
+            modelBuilder.Entity<Lifeevent>().Property(p => p.ReqPhotoPath).HasMaxLength(256);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
