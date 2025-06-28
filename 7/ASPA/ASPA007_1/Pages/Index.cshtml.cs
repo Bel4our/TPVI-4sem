b8303e822f3c0c42d20600794006394af77a4af4
Daniil <daniilsemenov435687@gmail.com>
2025-06-28 23:46:01 +0300

    seventh and eighth labs added

---
+++ 7/ASPA/ASPA007_1/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DAL_Celebrity_MSSQL;
+using System.Data.Common;
+using DAL_Celebrity_MSSQL;
+using DAL_Celebrity;
+namespace ASPA007_1.Pages;
+
+public class IndexModel : PageModel
+{
+    public List<Celebrity> cs = new List<Celebrity>();
+   
+
+    public void OnGet()
+    {
+        using (var db = new Repository(AppConfig.ConnectionString)) {
+            cs = db.GetAllCelebrities();
+        }
+    }
+}
